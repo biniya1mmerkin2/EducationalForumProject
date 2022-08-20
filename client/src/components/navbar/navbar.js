812f77f6073ae762657c13f2a31488abd1a1d49e
biniyam <biniyammerkin30@gmail.com>
2022-08-20 06:21:47 -0700

    updated the margin of the navigation bar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -6,25 +6,31 @@ import {
   Toolbar,
   Stack,
   Link,
+  Container,
 } from "@mui/material";
 
 const NavBar = () => {
   return (
-    <AppBar position="sticky" color="inherit" sx={{ background: "#1B1B1A" }}>
+    <AppBar
+      position="sticky"
+      color="inherit"
+      sx={{ background: "#080808", height: 150 }}
+    >
       <Toolbar>
         <Stack
           spacing={{ xs: 0, md: 30, lg: 40 }}
           direction={{ xs: "column", sm: "row", lg: "row" }}
+          m={10}
         >
           <Stack direction="row">
-            <Typography variant="h4" color="white" gutterBottom>
+            <Typography variant="h3" color="white" gutterBottom>
               Educational Forum
             </Typography>
           </Stack>
           <Stack
             direction={{ xs: "row", sm: "row", lg: "row" }}
-            sx={{ marginTop: 10 }}
             spacing={2}
+            m={40}
           >
             <Link href="#" underline="none" variant="inherit">
               Home
