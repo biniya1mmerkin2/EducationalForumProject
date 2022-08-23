c1a2af4fd110f7cc29d97d1f517e6e948561c3dd
biniyam <biniyammerkin30@gmail.com>
2022-08-23 08:10:11 -0700

    i added a table in the allposts page and my posts page simple table

--- client/src/components/forum/allposts/allposts.js
+++ client/src/components/forum/allposts/allposts.js
@@ -1,12 +1,158 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Stack,
+  Button,
+  Divider,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
 
 const Allposts = () => {
   return (
-    <Container maxWidth="md">
-      <Typography variant="h4" color="white">
-        All Posts Page
-      </Typography>
+    <Container
+      maxWidth="md"
+      sx={{
+        border: 1,
+        borderColor: "white",
+        height: 800,
+      }}
+    >
+      <Stack direction={"row"} spacing={38} m={1}>
+        <Stack>
+          <Typography variant="body1" color={"white"}>
+            sort by:Recently Activity
+          </Typography>
+        </Stack>
+        <Stack direction={"row"} spacing={1}>
+          <Button variant="outlined" color="warning">
+            {" "}
+            Mark all as Read
+          </Button>
+          <Button variant="contained" color="warning">
+            {" "}
+            Create New Post
+          </Button>
+        </Stack>
+      </Stack>
+      <Divider sx={{ border: 0.5, borderColor: "white", margin: 0 }} />
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Biniyam</TableCell>
+              <TableCell sx={{ color: "white" }}>Merkin</TableCell>
+              <TableCell sx={{ color: "white" }}>23</TableCell>
+              <TableCell sx={{ color: "white" }}>
+                Addis Abeba, Ethiopia
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
