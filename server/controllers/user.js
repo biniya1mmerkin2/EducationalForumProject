9a92e66d40418c57973e6378cdd7d54a3f9a932d
biniyam <biniyammerkin30@gmail.com>
2022-09-14 10:59:20 -0700

    setup the server for sending email

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 import bycrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
+import nodemailer from "nodemailer";
 
 export const signup = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
@@ -45,5 +46,43 @@ export const signin = async (req, res) => {
     res.status(200).json({ result: existinguser, token });
   } catch (error) {
     res.status(500).json(error);
+  }
+};
+
+export const forgetPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const existinguser = await User.findOne({ email });
+    if (!existinguser)
+      return res.status(404).json("user with this email number is not found!");
+
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    var mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "Password Reset Link",
+      text:
+        "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+        "Please click on the following link, or paste this into your browser to complete the process within 1 hour of receiving this message.\n\n" +
+        `http://localhost:3000/forgetpassword\n\n` +
+        "If you didn`t requested this, Please ignore this email and your password will remain unchanged.\n",
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        res.status(404).json(error);
+      } else {
+        res.status(200).json(info);
+      }
+    });
+  } catch (error) {
+    res.status(404).json(error);
   }
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signin, signup } from "../controllers/user.js";
+import { signin, signup, forgetPassword } from "../controllers/user.js";
 const user = express.Router();
 
 user.post("/signin", signin);
 user.post("/signup", signup);
+user.post("/forgetpassword", forgetPassword);
 
 export default user;
